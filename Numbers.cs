52e08908f4977bb82eb5bf48757c9c275157e17b
Anders Selborn <anders.selborn@wspgroup.se>
2017-09-04 10:25:30 +0200

    Add project files.

---
+++ Numbers.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cochlear
+{
+    static class Numbers
+    {
+        static string textString = "asd123qwe457rty89234";
+        static string textString2 = "567zx01245cvbnm";
+
+        static Dictionary<string, int> result = new Dictionary<string, int>();
+
+        public static void DoNumbers()
+        {
+
+            List<String> content = new List<string>();
+
+            content.Add(textString);
+            content.Add(textString2);
+
+            PrintMap(content);
+           
+        }
+
+        private static void PrintMap(List<string> inputs)
+        {
+            List<string> my = new List<string>();
+            StringBuilder bu = new StringBuilder();
+            Regex rgx = new Regex("[^Z0-9.]");
+
+            int start = 0;
+
+            List<String> inpStrings = new List<string>();
+            foreach(string input in inputs)
+            {
+                string filtered = rgx.Replace(input, "|");
+                inpStrings = filtered.Split(new char[] { '|' }).ToList();
+
+                foreach (String inp in inpStrings)
+                {
+                    if (inp.Length > 0)
+                    {
+                        int cnt = 0;
+                        while (cnt < inp.Length - 1)
+                        {
+                            if (cnt == 0)
+                            {
+                                start = Int32.Parse(inp.Substring(0, 1));
+                                bu.Append(start);
+                            }
+
+                            int next = Int32.Parse(inp.Substring(cnt + 1, 1));
+
+                            if (next - start == 1)
+                            {
+                                bu.Append(next);
+                                start = next;
+                            } else
+                            {
+                                int p = inp.IndexOf(next.ToString());
+                                if ( (inp.Length - p) != 1)
+                                {
+                                    start = Int32.Parse(inp.Substring(p, 1));
+                                    
+                                    my.Add(bu.ToString());
+                                    bu.Length = 0;
+                                    bu.Append(start);
+                                }
+
+                            }
+                            cnt++;
+                        }
+
+                        my.Add(bu.ToString());
+                        bu.Length = 0;
+                    }
+                }
+
+            }
+
+            List<int> resultInts = my.ConvertAll(p => Int32.Parse(p));
+
+            resultInts.Sort((a, b) => -1 * a.CompareTo(b));
+
+            //my = my.OrderBy(q => q).ToList();
+
+            var noGroups = resultInts.GroupBy(i => i).OrderByDescending(g => g.Count());
+            foreach (var grp in noGroups)
+            {
+                var number = grp.Key;
+                var total = grp.Count();
+
+                Console.WriteLine("{0}, {1}", number.ToString(), total.ToString());
+
+            }
+
+           
+        }
+
+
+
+
+    }
+}
